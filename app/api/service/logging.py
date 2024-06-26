0a6ac21a44b596fa87484f29b2c0f46a282760c4
vitaliitolokov <vitaliitolokov@gmail.com>
2024-06-26 22:31:59 +0500

    fix post endpoint

--- app/api/service/logging.py
+++ app/api/service/logging.py
@@ -5,21 +5,18 @@ from fastapi import Depends
 from app.database.core.engine import session
 from app.database.models.logs import Logs
 from fastapi import APIRouter
-from sqlalchemy import select, null
-
+from sqlalchemy import select, null, insert
 router = APIRouter()
 
 
 @router.get('/log', summary='Выводит сообщение из таблицы логирования', status_code=200)
 async def get_logs(db: AsyncSession = Depends(session)):
     logs = await db.scalars(select(Logs).where(Logs.id != null()).order_by(Logs.timestamp))
-    return {"result": logs.all()}, 200
+    return {"logs": logs.all()}, 200
 
 
 @router.post('/log', summary='Добавить запись в базу данных', status_code=200)
-async def post_add_log(message: str):
-    async with engine.begin() as conn:
-        query = f"""INSERT INTO logs (log_level, message, timestamp) VALUES ('INFO', '{message}' , '2024-06-22 08:08:08');"""
-        print(query)
-        await conn.execute(text(query))
-    return {'status': message}, 200
+async def post_add_log(message: str, db: AsyncSession = Depends(session)):
+    result = await db.scalar(insert(Logs).values(message=message).returning(Logs.id))
+    await db.commit()
+    return {'id': result}, 200
